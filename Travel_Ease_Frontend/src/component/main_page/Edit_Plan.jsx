967285696be6de0de8cc98db19ed4b26b71fba26
vaughn-joshua <dev.vaughnjoshua@gmail.com>
2025-10-25 19:22:18 +0800

    edit plan with date

--- Travel_Ease_Frontend/src/component/main_page/Edit_Plan.jsx
+++ Travel_Ease_Frontend/src/component/main_page/Edit_Plan.jsx
@@ -1,16 +1,55 @@
-import { useEffect } from "react";
 import { useForm } from "react-hook-form";
+import Flatpickr from "react-flatpickr";
+import "flatpickr/dist/flatpickr.css";
+import { useEffect, useState } from "react";
 
 function Edit_Plan({ data, on_close }) {
-  const { register, handleSubmit, reset } = useForm({
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
     defaultValues: {
       name: data[0].name,
       description: data[0].description,
       location: data[0].location,
       max_slots: data[0].max_slots,
       visibility: data[0].visibility,
+      start_date: data[0].start_date,
+      end_date: data[0].end_date,
     },
   });
+
+  const [dateRange, setDateRange] = useState([]);
+
+  const handle_change = (selectedDates, instance) => {
+    console.log(selectedDates);
+    if (selectedDates.length > 2) {
+      selectedDates.splice(2);
+      instance.setDate(selectedDates);
+    }
+
+    setDateRange([...selectedDates]);
+  };
+
+  useEffect(() => {
+    const formatDate = (number) => {
+      return (
+        dateRange[number] &&
+        dateRange[number].toLocaleDateString("en-US", {
+          month: "long",
+          day: "numeric",
+          year: "numeric",
+        })
+      );
+    };
+
+    reset({
+      start_date: formatDate(0),
+      end_date: formatDate(1),
+    });
+  }, [dateRange]);
 
   const handle_close = () => {
     on_close();
@@ -27,33 +66,64 @@ function Edit_Plan({ data, on_close }) {
       <form onSubmit={handleSubmit(on_submit)}>
         <label>
           Name:
-          <input {...register("name")} />
-        </label>{" "}
+          <input
+            {...register("name", {
+              required: "Name is Required",
+            })}
+          />
+        </label>
+        {errors.name && <p>{errors.name.message}</p>}
         <br />
         <label>
           Dsicription:
-          <input {...register("description")} />
-        </label>{" "}
+          <input
+            {...register("description", {
+              required: "Description is required.",
+            })}
+          />
+        </label>
+        {errors.description && <p>{errors.description.message}</p>}
         <br />
         <label>
           Visibility:
-          <input {...register("visibility")} />
-        </label>{" "}
+          <input type="checkbox" {...register("visibility")} />
+        </label>
+        {errors.visibility && <p>{errors.visibility.message}</p>}
         <br />
         <label>
           Slots:
-          <input {...register("max_slots")} />
-        </label>{" "}
+          <input
+            {...register("max_slots", {
+              required: "Slots is Required",
+            })}
+          />
+        </label>
+        {errors.max_slots && <p>{errors.max_slots.message}</p>}
         <br />
         <label>
           Location:
-          <input {...register("location")} />
-        </label>{" "}
+          <input
+            {...register("location", {
+              required: "Location is Required",
+            })}
+          />
+        </label>
+        {errors.location && <p>{errors.location.message}</p>}
         <br />
         <label>
-          Date:
-          <input {...register("date")} />
-        </label>{" "}
+          Dates:
+          <input {...register("start_date")} readOnly />
+          <input {...register("end_date")} disabled />
+        </label>
+        <br />
+        <Flatpickr
+          options={{
+            mode: "multiple",
+            dateFormat: "F j, Y",
+          }}
+          value={dateRange}
+          onChange={handle_change}
+        />{" "}
         <br />
         <input type="submit" value="Save" />
       </form>
